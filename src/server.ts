336b11d29d0bc5cf90686791ba1785cd6a0d9e80
Kevin McCarthy <kjmagic13@gmail.com>
2022-04-12 19:24:57 -0400

    force ssl

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,18 @@ import './discord'
 
 const PORT = process.env.PORT || 3000
 const app = express()
+
+// https redirect
+app.get('*', (req, res, next) => {
+  if (
+    process.env.NODE_ENV == 'production' &&
+    req.headers['x-forwarded-proto'] != 'https'
+  ) {
+    res.redirect(`https://${req.headers.host + req.url}`)
+  } else {
+    return next()
+  }
+})
+
 app.use(express.static('public'))
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
