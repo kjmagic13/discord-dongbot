664bcb6635f7ff4c4e67fc69b74471df55d776dc
Kevin <kjmagic13@gmail.com>
2024-02-16 08:49:07 -0500

    use nuxt pages

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Meta charset="UTF-8" />
+  <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <Link
+    rel="shortcut icon"
+    href="/assets/images/favicon.ico"
+    type="image/x-icon"
+  />
+  <Link
+    href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+    rel="stylesheet"
+  />
+
+  <main
+    class="min-h-screen bg-gray-800 py-6 flex flex-col justify-center sm:py-12"
+  >
+    <slot></slot>
+  </main>
+</template>
