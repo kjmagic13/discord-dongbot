5127dad95a5440db1ecdcf28f7d8f139df86b8ce
Kevin <kjmagic13@gmail.com>
2024-08-10 13:10:01 -0400

    use discord-interactions over tweetnacl

--- server/utils/validate-interaction.ts
+++ server/utils/validate-interaction.ts
@@ -1,5 +1,5 @@
-import { H3Event, H3Error } from "h3";
-import nacl from "tweetnacl";
+import { verifyKey } from "discord-interactions";
+import { H3Event } from "h3";
 
 const { DISCORD_PUBLIC_KEY } = process.env;
 
@@ -8,10 +8,11 @@ export async function validateDiscordInteraction<T = any>(event: H3Event) {
   const signature = getHeader(event, "X-Signature-Ed25519");
   const timestamp = getHeader(event, "X-Signature-Timestamp");
 
-  const isVerified = nacl.sign.detached.verify(
-    Buffer.from(timestamp ?? "" + body),
-    Buffer.from(signature ?? "", "hex"),
-    Buffer.from(DISCORD_PUBLIC_KEY ?? "", "hex")
+  const isVerified = await verifyKey(
+    body ?? "",
+    signature ?? "",
+    timestamp ?? "",
+    DISCORD_PUBLIC_KEY ?? ""
   );
 
   console.log({ body, signature, timestamp, isVerified });
