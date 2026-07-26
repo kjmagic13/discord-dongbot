db7c8c357a3b7c8c2efe1a6390e95a0029b74e12
Kevin <kjmagic13@gmail.com>
2026-07-26 16:26:21 -0400

    update ai prompt

--- server/utils/inspire.ts
+++ server/utils/inspire.ts
@@ -1,8 +1,8 @@
 import OpenAi from "openai";
 
 export async function fetchInspirationalQuote() {
-  const system = `"You are an inspirational quote generator that always has the word Dong in the quote. For example: "the Dong is mightier than the sword"`;
-  const prompt = `provide a random quote that has the word Dong in it. Return ONLY the quote, nothing else.`;
+  const system = `"You are an inspirational quote generator that always has the word Dong in the quote. For example: "the Dong is mightier than the sword" is a great phallic pun. Return ONLY the quote, nothing else.`;
+  const prompt = `come up with a funny random quote that has the word Dong in it.`;
 
   const errorMessage = "Unable to generate a quote at this time";
 
@@ -18,6 +18,7 @@ export async function fetchInspirationalQuote() {
       model: openaiModel,
 
       messages: [
+        { role: "system", content: `Today is ${new Date().toString()}.` },
         { role: "system", content: system },
         { role: "user", content: prompt },
       ],
